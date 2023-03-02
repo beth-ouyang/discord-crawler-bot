dbe7288c724f775fec9fd641d52f13d0b7233f8c
beth-ouyang <65527698+beth-ouyang@users.noreply.github.com>
2023-03-02 17:10:23 +0800

    add new features & use env variable

--- bot.py
+++ bot.py
@@ -1,10 +1,6 @@
 from discord.ext import commands 
 import discord
-import json
 from crawler import main as ad_crawler
-
-with open('items.json', "r", encoding = "utf8") as file:
-    data = json.load(file)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,17 +18,25 @@ async def on_message(message):
     #排除自己的訊息，避免陷入無限循環
     if message.author == client.user:
         return
-    #如果以「說」開頭
+
     if message.content.startswith('說'):
-      #分割訊息成兩份
-      tmp = message.content.split(" ",2)
-      #如果分割後串列長度只有1
+      tmp = message.content.split("說",2)
       if len(tmp) > 1:
-        await message.channel.send(tmp[1])
-        print(tmp[1])
-    elif message.content == "!申":
+        await message.channel.send(tmp[-1])
+        print(tmp[-1])
+    elif message.content=='申':
        admission_report = ad_crawler()
        await message.channel.send(admission_report)
        print(admission_report)
+    elif message.content.startswith('!line下載>>'):
+      tmp = message.content.replace(" ", "").split(">>",2)
+      line_emoji_download(tmp[-1])
+      await message.channel.send('下載好了!', file=discord.File("line_emoji.zip"))
+    elif message.content=='!指令':
+      await message.channel.send("""目前有的指令:
+      1. 說 + 想講的話 : 說安安 -> return 安安
+      2. 下載line貼圖/表情貼 : !line下載>> + line store網址 -> return zip檔
+      3. 查看最近米國研究所回報 : 申 -> return 結果
+      """)
 
-client.run(data['token'])
+client.run(os.environ['token'])
